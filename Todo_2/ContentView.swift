b8db500333526c6098dff42583617598121472ce
hiteshNath-Dev <hiteshsysorex@gmail.com>
2024-12-29 22:21:03 +0530

    Initial Commit

---
+++ Todo_2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Todo_2
+//
+//  Created by Administrator on 29/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Todo_2/Todo_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_2App.swift
+//  Todo_2
+//
+//  Created by Administrator on 29/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
